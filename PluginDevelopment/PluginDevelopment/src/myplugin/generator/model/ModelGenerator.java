6874ac7e364c1d1f1aa6f4af8bf841bb96b6aa9c
jaseyrae9 <jelena.surlan9@uns.ac.rs>
2020-07-23 12:38:20 +0200

    Importi za reference

--- PluginDevelopment/PluginDevelopment/src/myplugin/generator/model/ModelGenerator.java
+++ PluginDevelopment/PluginDevelopment/src/myplugin/generator/model/ModelGenerator.java
@@ -27,24 +27,24 @@ public class ModelGenerator extends PerClassGenerator {
 		context.put("package", fmClass.getTypePackage());
 		context.put("name", fmClass.getName());
 		context.put("tableName", fmClass.getTableName());
-		context.put("needsSet",
-				fmClass.getProperties().stream().anyMatch(p -> p.isCreateGetter() && p.getUpper() == -1));
+		context.put("needsSet", fmClass.getProperties().stream().anyMatch(p -> p.isCreateGetter() && p.getUpper() == -1));
 		context.put("needsSetterImport", fmClass.getProperties().stream().anyMatch(p -> p.isCreateSetter()));
 		context.put("needsGetterImport", fmClass.getProperties().stream().anyMatch(p -> p.isCreateGetter()));
 		context.put("imports", ImportUtil.uniqueTypesUsed(fmClass.getProperties(), true));
 
 		List<FMProperty> peristantProperties = fmClass.getProperties().stream()
 				.filter(p -> p instanceof FMPeristantProperty).collect(Collectors.toList());
+		List<FMProperty> linkedProperties = fmClass.getProperties().stream().filter(p -> p instanceof FMLinkedProperty)
+				.collect(Collectors.toList());
 		
 		context.put("peristantProperties", peristantProperties);
-		context.put("linkedProperties", fmClass.getProperties().stream().filter(p -> p instanceof FMLinkedProperty)
-				.collect(Collectors.toList()));
+		context.put("linkedProperties", linkedProperties);
 		context.put("properties", fmClass.getProperties().stream().filter(p -> !p.isPersistant()).collect(Collectors.toList()));
 	
-		context.put("persistenceImports", persistenceImports(fmClass.getTableName(), peristantProperties));
+		context.put("persistenceImports", persistenceImports(fmClass.getTableName(), peristantProperties, linkedProperties));
 	}
 
-	public static Set<String> persistenceImports(String tableName, List<FMProperty> properties){
+	public static Set<String> persistenceImports(String tableName, List<FMProperty> pp, List<FMProperty> lp){
 		Set<String> imports = new HashSet<String>();
 		imports.add("import javax.persistence.Entity;");
 
@@ -52,7 +52,7 @@ public class ModelGenerator extends PerClassGenerator {
 			imports.add("import javax.persistence.Table;");
 		}
 		
-		List<FMPeristantProperty> persistantProperties = properties.stream()
+		List<FMPeristantProperty> persistantProperties = pp.stream()
 				.filter(p-> p instanceof FMPeristantProperty)
 				.map(p -> (FMPeristantProperty)p).collect(Collectors.toList());
 		
@@ -68,7 +68,30 @@ public class ModelGenerator extends PerClassGenerator {
 		if(persistantProperties.stream().anyMatch(p -> p.getUpper() == 1)) {
 			imports.add("import javax.persistence.Column;");
 		}
-				
+		
+		List<FMLinkedProperty> linkedProperties = lp.stream()
+				.filter(p-> p instanceof FMLinkedProperty)
+				.map(p -> (FMLinkedProperty)p).collect(Collectors.toList());
+		
+		if(linkedProperties.stream().anyMatch(p ->  p.getUpper() == -1 && p.getOppositeEnd().getUpper()== -1 )) { //manytomany
+			imports.add("import javax.persistence.ManyToMany;");
+		}
+		if(linkedProperties.stream().anyMatch(p ->  p.getUpper() == -1 && p.getOppositeEnd().getUpper()== 1 )) { //OneToMany
+			imports.add("import javax.persistence.OneToMany;");
+		}
+		if(linkedProperties.stream().anyMatch(p ->  p.getUpper() == 1 && p.getOppositeEnd().getUpper()== -1 )) { //ManyToOne
+			imports.add("import javax.persistence.ManyToOne;");
+		}
+		if(linkedProperties.stream().anyMatch(p ->  p.getUpper() == 1 && p.getOppositeEnd().getUpper()== 1 )) { //OneToOne
+			imports.add("import javax.persistence.OneToOne;");
+		}
+		if(linkedProperties.stream().anyMatch(p -> p.getFetch() != null)) { 
+			imports.add("import javax.persistence.FetchType;");
+		}
+		if(linkedProperties.stream().anyMatch(p -> p.getCascade() != null)) { 
+			imports.add("import javax.persistence.CascadeType;");
+		}
+
 		return imports;
 	}
 }
